7902e4fa85204305e4406152d6eb26dbd93e734d
JohnSweetall <jts6052@psu.edu>
2020-09-04 22:14:01 +0000

    changes

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
+// Author: John Sweetall jts6052@psu.edu
+// Collaborator: Yash Patni yjp5077@psu.edu
+// Collaborator: Angus Hendrick ajh97@psu.edu
+// Collaborator: Angela Bao ymb5072@psu.edu
+
+
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
 
 int main(void) {
 
-double tempC;
-double tempF;
+char *Celsius = readline("Enter temperature in celsius: ");
 
-char * Celsius = readline("Enter temperature in celsius: ");
+double tempC = atof(Celsius);
 
-tempC = atof(Celsius);
-tempF = ((tempC*9/5) + 32);
-
-printf("%f° in celsius is euqivalent to %f° Fahrenheit.\n", tempC, tempF);
+printf("%f° in celsius is euqivalent to %f° Fahrenheit.\n", tempC, tempC*9/5 + 32);
 
 return 0;
 
